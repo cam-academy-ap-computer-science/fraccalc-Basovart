642546f5e681127cff113a76645d0537fef7aec5
basovart000 <basovart000@CAM-LAB-11.bgsd.k12.wa.us>
2019-12-06 07:50:52 -0800

    inProg.

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,4 +1,5 @@
 /*PSEUDOCODE
+ * CHECKPOINT 1:
  * Create scanner 
  * have the input be passed to produceAnswer
  * break up produceAnswer into three strings
@@ -8,6 +9,10 @@
  * store these in produceAnswer
  * produceAnswer return the second operand
  * main prints the return value
+ * CHECKPOINT 2:
+ * accept multiple inputs until user types 'quit'
+ * parse operand into Whole, Numerator, and Denominator
+ * 
  */
 package fracCalc;
 import java.util.Scanner;
@@ -15,9 +20,15 @@ public class FracCalc {
     public static void main(String[] args) 
     {
     	Scanner userInput = new Scanner(System.in);
-    	String input = userInput.nextLine();
+    	String input1 = "";
+
+    	do {String input = userInput.nextLine();
     	produceAnswer(input);
     	System.out.println();
+    	} while (input1 != "quit"); 
+    	if (input1 == "quit") {
+    		System.out.println("Good Bye");
+    	}
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -28,31 +39,29 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static void produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input) { 
     	
     	int length = input.length();
     	int space = input.indexOf(" ");
     	int x = space + 3;
-    	
     	String firstOperand = input.substring(0 ,space);
     	String operator = input.substring(space, space + 2);
     	String secondOperand = input.substring(x, length);
         
-    	String Whole;
-    	String Numerator;
-    	String Denominator;
-    	
-        if (input.indexOf("_") == -1 && input.indexOf("/") == -1) {
+        String Whole = "";
+		String Numerator = "";	
+		String Denominator = "";
+		
+		if (secondOperand.indexOf("_") == -1 && secondOperand.indexOf("/") == -1) {
         	Whole = secondOperand;
         	Numerator = "0";
         	Denominator = "1";
-        	
         }
-        System.out.print("Whole: " + Whole + " Numerator: " + Numerator + " Denominator: " + Denominator);
+		System.out.print("whole:" + Whole + " numerator:" + Numerator + " denominator:" + Denominator);
        
+		return secondOperand;
     }
-
+    
     // TODO: Fill in the space below with any helper methods that you think you will need
     
 }
