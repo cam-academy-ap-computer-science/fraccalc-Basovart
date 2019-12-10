728b5b8e4f4698ed4b889288dbccd5bd11b690f0
artyom <artyom@localhost>
2019-12-10 07:01:18 -0800

    why.

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -33,8 +33,7 @@ public class FracCalc {
     		System.out.println("Good Bye");
     	} 
     }
-    
-    
+        
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
     //
@@ -43,6 +42,7 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
+    
     public static String produceAnswer(String input) { 
     	
     	int length = input.length();
@@ -51,27 +51,47 @@ public class FracCalc {
     	String firstOperand = input.substring(0 ,space);
     	char operator = input.charAt(space + 1);
     	String secondOperand = input.substring(secondSpace + 1, length);
+    	int firstUnder = input.indexOf("_");
+    	int firstDiv = input.indexOf("/");
         int secondUnder = input.lastIndexOf("_");
         int secondDiv = input.lastIndexOf("/");
-        String Whole = "";
-		String Numerator = "";	
-		String Denominator = "";
-		
-		if (secondOperand.indexOf("_") == -1 && secondOperand.indexOf("/") == -1) {
-        	Whole = secondOperand;
-        	Numerator = "0";
-        	Denominator = "1";
-        } else if (secondOperand.indexOf("_") != -1 && secondOperand.indexOf("/") != -1) {
-			Whole = input.substring(secondSpace+1,secondUnder);	
-			Numerator = input.substring(secondUnder+1,secondDiv);
-			Denominator = input.substring(secondDiv+1,length);
-  		  } else if (secondOperand.indexOf("_") == -1 && input.lastIndexOf("/") != -1) {
-			Whole = "0";
-			Numerator = (input.substring(secondSpace + 1,secondDiv));
-			Denominator = (input.substring(secondDiv+1,length));
+        String Whole1 = "";
+        String Numerator1 = "";
+        String Denominator1 = "";
+        String Whole2 = "";
+		String Numerator2 = "";	
+		String Denominator2 = "";
+		/*
+		if (firstOperand.indexOf("_") == -1 && firstOperand.indexOf("/") == -1) {
+        	Whole1 = firstOperand;
+        	Numerator1 = "0";
+        	Denominator1 = "1";
+        } else if (firstOperand.indexOf("_") != -1 && firstOperand.indexOf("/") != -1) {
+			Whole1 = input.substring(0,firstUnder);	
+			Numerator1 = input.substring(firstUnder+1,firstDiv);
+			Denominator1 = input.substring(firstDiv+1,space);
+  		  } else if (firstOperand.indexOf("_") == -1 && input.lastIndexOf("/") != -1) {
+			Whole1 = "0";
+			Numerator1 = input.substring(0,firstDiv);
+			Denominator1 = input.substring(firstDiv+1,space);
 		    }
+		System.out.println("whole:" + Whole1 + " numerator:" + Numerator1 + " denominator:" + Denominator1);
+		*/
+		if (secondOperand.indexOf("_") == -1 && secondOperand.indexOf("/") == -1) {
+        	Whole2 = secondOperand;
+        	Numerator2 = "0";
+        	Denominator2 = "1";
+        } else if (secondOperand.indexOf("_") != -1 && secondOperand.indexOf("/") != -1) {
+			Whole2 = input.substring(secondSpace+1,secondUnder);	
+			Numerator2 = input.substring(secondUnder+1,secondDiv);
+			Denominator2 = input.substring(secondDiv+1,length);
+  		  } else if (secondOperand.indexOf("_") == -1 && input.lastIndexOf("/") != -1) {
+			Whole2 = "0";
+			Numerator2 = input.substring(secondSpace + 1,secondDiv);
+			Denominator2 = input.substring(secondDiv+1,length);
+		    }
+		System.out.println("whole:" + Whole2 + " numerator:" + Numerator2 + " denominator:" + Denominator2);
 		
-		System.out.println("\"whole:" + Whole + " numerator:" + Numerator + " denominator:" + Denominator);
 		return secondOperand;
     }
     
