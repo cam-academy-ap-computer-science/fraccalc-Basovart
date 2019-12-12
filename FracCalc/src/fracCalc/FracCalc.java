c5921588e138035282de531bfd4df5507b8edfb5
artyom <artyom@localhost>
2019-12-12 07:02:11 -0800

    Checkpoint2Done.

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -32,6 +32,8 @@ public class FracCalc {
     	if (input.equals("quit")) {
     		System.out.println("Good Bye");
     	} 
+    	int answer;
+		//System.out.println(add(answer));
     }
         
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -55,46 +57,80 @@ public class FracCalc {
     	int firstDiv = input.indexOf("/");
         int secondUnder = input.lastIndexOf("_");
         int secondDiv = input.lastIndexOf("/");
-        String Whole1 = "";
-        String Numerator1 = "";
-        String Denominator1 = "";
-        String Whole2 = "";
-		String Numerator2 = "";	
-		String Denominator2 = "";
-		/*
+        int Whole1 = 0;
+        int Numerator1 = 0;
+        int Denominator1 = 0;
+        int Whole2 = 0;
+		int Numerator2 = 0;	
+		int Denominator2 = 0;
+		
 		if (firstOperand.indexOf("_") == -1 && firstOperand.indexOf("/") == -1) {
-        	Whole1 = firstOperand;
-        	Numerator1 = "0";
-        	Denominator1 = "1";
+        	Whole1 = Integer.parseInt(firstOperand);
+        	Numerator1 = 0;
+        	Denominator1 = 1;
         } else if (firstOperand.indexOf("_") != -1 && firstOperand.indexOf("/") != -1) {
-			Whole1 = input.substring(0,firstUnder);	
-			Numerator1 = input.substring(firstUnder+1,firstDiv);
-			Denominator1 = input.substring(firstDiv+1,space);
+			Whole1 = Integer.parseInt(input.substring(0,firstUnder));	
+			Numerator1 = Integer.parseInt(input.substring(firstUnder+1,firstDiv));
+			Denominator1 = Integer.parseInt(input.substring(firstDiv+1,space));
   		  } else if (firstOperand.indexOf("_") == -1 && input.lastIndexOf("/") != -1) {
-			Whole1 = "0";
-			Numerator1 = input.substring(0,firstDiv);
-			Denominator1 = input.substring(firstDiv+1,space);
+			Whole1 = 0;
+			Numerator1 = Integer.parseInt(input.substring(0,firstDiv));
+			Denominator1 = Integer.parseInt(input.substring(firstDiv+1,space));
 		    }
 		System.out.println("whole:" + Whole1 + " numerator:" + Numerator1 + " denominator:" + Denominator1);
-		*/
+	
 		if (secondOperand.indexOf("_") == -1 && secondOperand.indexOf("/") == -1) {
-        	Whole2 = secondOperand;
-        	Numerator2 = "0";
-        	Denominator2 = "1";
+        	Whole2 = Integer.parseInt(secondOperand);
+        	Numerator2 = 0;
+        	Denominator2 = 1;
         } else if (secondOperand.indexOf("_") != -1 && secondOperand.indexOf("/") != -1) {
-			Whole2 = input.substring(secondSpace+1,secondUnder);	
-			Numerator2 = input.substring(secondUnder+1,secondDiv);
-			Denominator2 = input.substring(secondDiv+1,length);
+			Whole2 = Integer.parseInt(input.substring(secondSpace+1,secondUnder));	
+			Numerator2 = Integer.parseInt(input.substring(secondUnder+1,secondDiv));
+			Denominator2 = Integer.parseInt(input.substring(secondDiv+1,length));
   		  } else if (secondOperand.indexOf("_") == -1 && input.lastIndexOf("/") != -1) {
-			Whole2 = "0";
-			Numerator2 = input.substring(secondSpace + 1,secondDiv);
-			Denominator2 = input.substring(secondDiv+1,length);
+			Whole2 = 0;
+			Numerator2 = Integer.parseInt(input.substring(secondSpace + 1,secondDiv));
+			Denominator2 = Integer.parseInt(input.substring(secondDiv+1,length));
 		    }
-		System.out.println("whole:" + Whole2 + " numerator:" + Numerator2 + " denominator:" + Denominator2);
-		
+		secondOperand = ("whole:" + Whole2 + " numerator:" + Numerator2 + " denominator:" + Denominator2);
+		/*
+		if (input.indexOf(" + ") != -1) {
+			add(Whole1, Whole2, Numerator1, Numerator2, Denominator1, Denominator2);
+		}
+		if (input.indexOf(" - ") != -1) {
+			sub(Whole1, Whole2, Numerator1, Numerator2, Denominator1, Denominator2);
+		}
+		if (input.indexOf(" * ") != -1) {
+			mul(Whole1, Whole2, Numerator1, Numerator2, Denominator1, Denominator2);
+		}
+		if (input.indexOf(" / ") != -1) {
+			div(Whole1, Whole2, Numerator1, Numerator2, Denominator1, Denominator2);
+		}
+		*/
 		return secondOperand;
     }
+    /*
+    public static String add(int Whole1) {
+    	int finalWhole = Whole1 + Whole2;
+    	int finalNumerator = Numerator1 + Numerator2;
+    	int finalDenominator = Denominator1 + Denominator2;
+    	String answer = (finalWhole + "_" + finalNumerator + "/" + finalDenominator);
+    	return answer;
+    }
     
+    public static String sub(int Whole1,int Whole2,int Numerator1, int Numerator2, int Denominator1, int Denominator2) {
+    	
+    	return answer;
+    }
+    public static String mul(int Whole1,int Whole2,int Numerator1, int Numerator2, int Denominator1, int Denominator2) {
+    	
+    	return answer;
+    }
+    public static String div(int Whole1,int Whole2,int Numerator1, int Numerator2, int Denominator1, int Denominator2) {
+    	
+    	return answer;
+    }
+    */
     // TODO: Fill in the space below with any helper methods that you think you will need
     
 }
