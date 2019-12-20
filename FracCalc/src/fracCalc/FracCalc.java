8e2011b4dbcb0fffa11f3aec978a91453139a6a6
artyom <artyom@localhost>
2019-12-20 06:56:58 -0800

    Ckeck2Done.

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -32,8 +32,8 @@ public class FracCalc {
     	if (input.equals("quit")) {
     		System.out.println("Good Bye");
     	} 
-    	int answer;
-		//System.out.println(add(answer));
+    	
+		
     }
         
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -51,7 +51,7 @@ public class FracCalc {
     	int space = input.indexOf(" ");
     	int secondSpace = input.lastIndexOf(" ");
     	String firstOperand = input.substring(0 ,space);
-    	char operator = input.charAt(space + 1);
+    	String operator = input.substring(space, space + 3);
     	String secondOperand = input.substring(secondSpace + 1, length);
     	int firstUnder = input.indexOf("_");
     	int firstDiv = input.indexOf("/");
@@ -78,6 +78,7 @@ public class FracCalc {
 			Denominator1 = Integer.parseInt(input.substring(firstDiv+1,space));
 		    }
 		System.out.println("whole:" + Whole1 + " numerator:" + Numerator1 + " denominator:" + Denominator1);
+		System.out.println(operator);
 	
 		if (secondOperand.indexOf("_") == -1 && secondOperand.indexOf("/") == -1) {
         	Whole2 = Integer.parseInt(secondOperand);
@@ -93,7 +94,7 @@ public class FracCalc {
 			Denominator2 = Integer.parseInt(input.substring(secondDiv+1,length));
 		    }
 		secondOperand = ("whole:" + Whole2 + " numerator:" + Numerator2 + " denominator:" + Denominator2);
-		/*
+		
 		if (input.indexOf(" + ") != -1) {
 			add(Whole1, Whole2, Numerator1, Numerator2, Denominator1, Denominator2);
 		}
@@ -106,15 +107,45 @@ public class FracCalc {
 		if (input.indexOf(" / ") != -1) {
 			div(Whole1, Whole2, Numerator1, Numerator2, Denominator1, Denominator2);
 		}
-		*/
+		
 		return secondOperand;
     }
-    /*
-    public static String add(int Whole1) {
-    	int finalWhole = Whole1 + Whole2;
-    	int finalNumerator = Numerator1 + Numerator2;
-    	int finalDenominator = Denominator1 + Denominator2;
-    	String answer = (finalWhole + "_" + finalNumerator + "/" + finalDenominator);
+    
+    public static String add(int Whole1, int Whole2, int Numerator1, int Numerator2, int Denominator1, int Denominator2) {
+    	int Num1;
+    	int Num2;
+    	int Den1;
+    	int Den2;
+    	int numFinal = 0;
+    	int denFinal = 0;
+    	int finalWhole;
+    	if (Denominator1 != Denominator2) {
+    		Num1 = Numerator1 * Denominator2;
+    		Num2 = Numerator2 * Denominator1;
+    		Den1 = Denominator1 * Denominator2;
+    		Den2 = Denominator2 * Denominator1;
+    		numFinal = Num1 + Num2;
+    		denFinal = Den1 + Den2;
+    	}
+    	if (Denominator1 == Denominator2) {
+    		numFinal = Numerator1 + Numerator2;
+    		denFinal = Denominator1 + Denominator2;
+    	}
+    //Whole
+    	if (Whole1 == -1 && Whole2 == -1) {
+    		finalWhole = 0;
+    	} else {
+    		finalWhole = Whole1 + Whole2;
+    	  }
+    	if (Whole1 != -1 && Whole2 == -1) {
+    		finalWhole = Whole1;
+    	} else {
+    		finalWhole = Whole2;
+    	  }
+    	
+    	
+    	String answer = (finalWhole + numFinal + "/" + denFinal);
+    	System.out.println(answer);
     	return answer;
     }
     
@@ -130,7 +161,7 @@ public class FracCalc {
     	
     	return answer;
     }
-    */
+    
     // TODO: Fill in the space below with any helper methods that you think you will need
     
 }
